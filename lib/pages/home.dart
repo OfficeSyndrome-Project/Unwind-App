30a89ae3efd24248d5e82fda8597a20fbb498384
Samach Weerakam <samach21@gmail.com>
2024-01-21 17:00:34 +0700

    add screen back

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,7 +3,6 @@ import 'package:unwind_app/pages/screening-feature/get_started_screening_page.da
 import 'package:unwind_app/pages/history-feature/history_page.dart';
 import 'package:unwind_app/pages/menu_page.dart';
 import 'package:unwind_app/pages/profile-feature/profile_page.dart';
-import 'package:unwind_app/pages/testingtts_page.dart';
 import 'package:unwind_app/pages/workoutList-feature/workoutlist_page.dart';
 
 class HomePage extends StatefulWidget {
@@ -18,7 +17,7 @@ class _HomeState extends State<HomePage> {
   late int _selectedIndex = widget.selectedIndex;
 
   final List<Widget> _widgetOptions = <Widget>[
-    TestingTTFPage(),
+    const ScreeningPage(),
     WorkoutListPage(),
     const MenuWidget(),
     HistoryPage(),
