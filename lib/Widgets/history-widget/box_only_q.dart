585950b12ba3bb99a5c2b7e777bba7d5a28b5d8b
Kukul <naththanicha@gmail.com>
2024-02-12 03:49:12 +0700

    update result history

--- lib/Widgets/history-widget/box_only_q.dart
+++ lib/Widgets/history-widget/box_only_q.dart
@@ -8,54 +8,58 @@ class BoxOnlyQ extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          margin: EdgeInsets.only(left: 8),
-          child: Text(
-            '${question}',
-            style: TextStyle(
-              fontSize: ResponsiveCheckWidget.isSmallMobile(context) ? 14 : 16,
-              fontWeight: FontWeight.w500,
-              color: const Color(0xFF484D56),
+    return Container(
+      margin: EdgeInsets.only(bottom: 8),
+      child: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(left: 8),
+            child: Text(
+              '${question}',
+              style: TextStyle(
+                fontSize:
+                    ResponsiveCheckWidget.isSmallMobile(context) ? 14 : 16,
+                fontWeight: FontWeight.w500,
+                color: const Color(0xFF484D56),
+              ),
             ),
           ),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Flexible(
-              flex: 1,
-              child: Text(
-                'ตอบ',
-                style: TextStyle(
-                  fontSize:
-                      ResponsiveCheckWidget.isSmallMobile(context) ? 14 : 16,
-                  fontWeight: FontWeight.w600,
-                  color: const Color(0xFF484D56),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Flexible(
+                flex: 1,
+                child: Text(
+                  'ตอบ',
+                  style: TextStyle(
+                    fontSize:
+                        ResponsiveCheckWidget.isSmallMobile(context) ? 14 : 16,
+                    fontWeight: FontWeight.w600,
+                    color: const Color(0xFF484D56),
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
-              width: 8,
-            ),
-            Flexible(
-              flex: 5,
-              fit: FlexFit.tight,
-              child: Text(
-                answer,
-                style: TextStyle(
-                  fontSize:
-                      ResponsiveCheckWidget.isSmallMobile(context) ? 14 : 16,
-                  fontWeight: FontWeight.w500,
-                  color: const Color(0xFF484D56),
-                ),
+              SizedBox(
+                width: 8,
               ),
-            )
-          ],
-        )
-      ],
+              Flexible(
+                flex: 5,
+                fit: FlexFit.tight,
+                child: Text(
+                  answer,
+                  style: TextStyle(
+                    fontSize:
+                        ResponsiveCheckWidget.isSmallMobile(context) ? 14 : 16,
+                    fontWeight: FontWeight.w500,
+                    color: const Color(0xFF484D56),
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
